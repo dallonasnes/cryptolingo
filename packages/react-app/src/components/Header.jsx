5f60292f813b63e41bc1c3ed61ef0d7f038e23e2
Dallon Asnes <dallon.asnes@gmail.com>
2022-03-11 12:11:51 -0300

    Add barebones read and upload components

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -3,17 +3,14 @@ import React from "react";
 
 // displays a page header
 
-export default function Header({ auth }) {
+export default function Header() {
   return (
-    <>
-      <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
-        <PageHeader
-          title="🏗 scaffold-eth"
-          subTitle="forkable Ethereum dev stack focused on fast product iteration"
-          style={{ cursor: "pointer" }}
-        />
-      </a>
-      <div>{auth ? `Your session is authenticated` : `Your session is NOT authenticated`}</div>
-    </>
+    <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
+      <PageHeader
+        title="CryptoLingo"
+        subTitle="Get paid in crypto to learn a new language or upload content in your native language"
+        style={{ cursor: "pointer" }}
+      />
+    </a>
   );
 }
