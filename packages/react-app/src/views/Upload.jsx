d302ecaef96f92a73136a818d10872ae8d12ef0b
Dallon Asnes <dallon.asnes@gmail.com>
2022-03-18 01:49:57 -0300

    Upload audio + handles edge cases

--- packages/react-app/src/views/Upload.jsx
+++ packages/react-app/src/views/Upload.jsx
@@ -6,75 +6,6 @@ import { create } from "ipfs-http-client";
 
 const client = create("https://ipfs.infura.io:5001/api/v0");
 
-const handleSuccess = stream => {
-  const player = document.getElementById("player");
-  const downloadLink = document.getElementById("download");
-  const startButton = document.getElementById("start");
-  const stopButton = document.getElementById("stop");
-  const options = { mimeType: "audio/webm" };
-  const recordedChunks = [];
-  const mediaRecorder = new MediaRecorder(stream, options);
-
-  // Element to append to dom for downloading audio
-  let a;
-
-  mediaRecorder.addEventListener("dataavailable", e => {
-    e.preventDefault();
-    if (e.data.size > 0) {
-      recordedChunks.push(e.data);
-    }
-  });
-
-  mediaRecorder.addEventListener("stop", e => {
-    e.preventDefault();
-
-    const blob = new Blob(recordedChunks, {
-      type: "audio/webm",
-    });
-    const url = URL.createObjectURL(blob);
-    a = document.createElement("a");
-    document.body.appendChild(a);
-    a.style = "display: none";
-    a.href = url;
-    a.download = "yourAudio.wav";
-    player.src = url;
-  });
-
-  mediaRecorder.addEventListener("error", e => {
-    console.log("ERROR:", e);
-  });
-
-  stopButton.addEventListener("click", event => {
-    debugger;
-    event.preventDefault();
-    mediaRecorder.stop();
-  });
-
-  // startButton.addEventListener("click", event => {
-  //   event.preventDefault();
-
-  //   // // If "a" element exists, revoke it's url and start over
-  //   // if (a) {
-  //   //   window.URL.revokeObjectURL(a.href);
-  //   //   a = undefined;
-  //   // }
-
-  //   // // If player source is set to a.href, that's gone now
-  //   // if (player && player.src) {
-  //   //   player.src = undefined;
-  //   // }
-
-  //   // mediaRecorder.start();
-  // });
-
-  downloadLink.addEventListener("click", () => {
-    if (!a) alert("No uploaded audio to download");
-    // Download
-    a.click();
-  });
-
-  mediaRecorder.start();
-};
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
  * @param {*} yourLocalBalance balance on current network
@@ -82,8 +13,9 @@ const handleSuccess = stream => {
  * @returns react component
  **/
 function Upload({ yourLocalBalance, readContracts, auth, writeContracts, tx }) {
-  const [text, setText] = useState();
-  const [textFileUrl, setTextFileUrl] = useState();
+  const [text, setText] = useState("");
+  const [textFileUrl, setTextFileUrl] = useState("");
+  const [didStartRecording, setDidStartRecording] = useState(false);
 
   const handleSubmit = async () => {
     if (!text) return;
@@ -95,6 +27,66 @@ function Upload({ yourLocalBalance, readContracts, auth, writeContracts, tx }) {
     // 2. setup livepeer and record
     // 3. write both to NFT storage api, await response
     // 4. write to createStory api on smart contract
+  };
+
+  const handleSuccess = stream => {
+    const player = document.getElementById("player");
+    const downloadLink = document.getElementById("download");
+    const startButton = document.getElementById("start");
+    const stopButton = document.getElementById("stop");
+    const options = { mimeType: "audio/webm" };
+    const recordedChunks = [];
+    const mediaRecorder = new MediaRecorder(stream, options);
+
+    // Element to append to dom for downloading audio
+    let a;
+
+    mediaRecorder.addEventListener("dataavailable", e => {
+      e.preventDefault();
+      if (e.data.size > 0) {
+        recordedChunks.push(e.data);
+      }
+    });
+
+    mediaRecorder.addEventListener("stop", e => {
+      e.preventDefault();
+
+      const blob = new Blob(recordedChunks, {
+        type: "audio/webm",
+      });
+      const url = URL.createObjectURL(blob);
+      a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      a.href = url;
+      a.download = "yourAudio.wav";
+      player.src = url;
+    });
+
+    mediaRecorder.addEventListener("error", e => {
+      console.log("ERROR:", e);
+    });
+
+    stopButton.addEventListener("click", event => {
+      event.preventDefault();
+      try {
+        mediaRecorder.stop();
+      } catch (e) {
+        console.log("ERROR:", e);
+        alert("No ongoing media recording. Refresh the page to start a new one");
+      }
+    });
+
+    downloadLink.addEventListener("click", () => {
+      if (!a) {
+        alert("No uploaded audio to download");
+        return;
+      }
+      // Download
+      a.click();
+    });
+
+    mediaRecorder.start();
   };
 
   const handleChange = event => {
@@ -110,7 +102,14 @@ function Upload({ yourLocalBalance, readContracts, auth, writeContracts, tx }) {
       <br />
       <button
         id="start"
-        onClick={() => navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess)}
+        onClick={() => {
+          if (didStartRecording) {
+            alert("Already made a recording. Refresh the page to start a new one");
+            return;
+          }
+          setDidStartRecording(true);
+          navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
+        }}
       >
         Start
       </button>
