3e939f946ccc618f930659341fa210f94dedbe20
Dallon Asnes <dallon.asnes@gmail.com>
2022-03-18 23:24:09 -0300

    Use more efficient data structures for lookups

--- packages/react-app/src/views/Read.jsx
+++ packages/react-app/src/views/Read.jsx
@@ -12,7 +12,7 @@ import { ethers } from "ethers";
  **/
 function Read({ address, yourLocalBalance, readContracts, auth, writeContracts, tx, tokenBalance, setTokenBalance }) {
   /*
-   * 1. First need to fetch list of all stories CID pairs from Contract
+   * 1. First fetch list of all stories CID pairs from Contract
    * 2. Fetch and see which are owned already by this wallet
    * 3. Then fetch beginning of text from IPFS for each and render
    *      (use helper methods to read and write in case we migrate)
@@ -22,7 +22,7 @@ function Read({ address, yourLocalBalance, readContracts, auth, writeContracts, 
    *      Else show "you haven't purchased"
    */
 
-  const [storyPreviewMetadatas, setStoryPreviewMetadatas] = useState([]);
+  const [storyPreviewMetadatas, setStoryPreviewMetadatas] = useState({});
   /*
     {
       id: "<text>:<audio>",
@@ -31,7 +31,7 @@ function Read({ address, yourLocalBalance, readContracts, auth, writeContracts, 
       author: address,
     }
    */
-  const [storyPreviews, setStoryPreviews] = useState([]);
+  const [storyPreviews, setStoryPreviews] = useState({});
   /*
     {
       id: "<text><audio>",
@@ -46,38 +46,42 @@ function Read({ address, yourLocalBalance, readContracts, auth, writeContracts, 
 
   useEffect(() => {
     async function fetchStories() {
-      // TODO: uncomment ME after DEBUGGING
-      // if (readContracts && readContracts.CryptoLingo && storyPreviewMetadatas.length > 0) {
-      //   try {
-      //     const res = await readContracts.CryptoLingo.getStories(address);
-      //     if (res && res.stories && res.stories.length > 0) {
-      //       // TODO: sort by net upvote count
-      //       setStoryPreviewMetadatas(res.stories);
-      //     }
-      //   } catch (e) {
-      //     console.log("ERR:", e);
-      //   }
-      // }
-      const tmp = [
-        {
-          id: "bafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5ybafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7qohj5mq3sjj4g7v5y",
-          upvoteCount: 5,
-          downvoteCount: 2,
-          author: address,
-        },
-        {
-          id: "bafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5ybafybeiaoporrmlqo6vpn4z52hfvpaavknxyrq8777rohj5mq3sjj4g7v5y",
-          upvoteCount: 9,
-          downvoteCount: 12,
-          author: address,
-        },
-        {
-          id: "bafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5ybafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5y",
-          upvoteCount: 88,
-          downvoteCount: 2,
-          author: address,
-        },
-      ];
+      if (readContracts && readContracts.CryptoLingo) {
+        try {
+          const res = await readContracts.CryptoLingo.getStories(address);
+          if (res && res.stories && res.stories.length > 0) {
+            const dict = {};
+            res.stories.map(obj => (dict[id] = obj));
+            setStoryPreviewMetadatas(dict);
+          }
+        } catch (e) {
+          console.log("ERR:", e);
+        }
+      }
+      // TODO: DELETE ME after DEBUGGING
+      const tmp = {
+        bafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5ybafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7qohj5mq3sjj4g7v5y:
+          {
+            id: "bafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5ybafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7qohj5mq3sjj4g7v5y",
+            upvoteCount: 5,
+            downvoteCount: 2,
+            author: address,
+          },
+        bafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5ybafybeiaoporrmlqo6vpn4z52hfvpaavknxyrq8777rohj5mq3sjj4g7v5y:
+          {
+            id: "bafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5ybafybeiaoporrmlqo6vpn4z52hfvpaavknxyrq8777rohj5mq3sjj4g7v5y",
+            upvoteCount: 9,
+            downvoteCount: 12,
+            author: address,
+          },
+        bafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5ybafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5y:
+          {
+            id: "bafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5ybafybeiaoporrmlqo6vpn4z52hfvpaavknxyrqi7g7rohj5mq3sjj4g7v5y",
+            upvoteCount: 88,
+            downvoteCount: 2,
+            author: address,
+          },
+      };
       setStoryPreviewMetadatas(tmp);
     }
     fetchStories();
@@ -86,11 +90,12 @@ function Read({ address, yourLocalBalance, readContracts, auth, writeContracts, 
   useEffect(() => {
     // To fetch text previews for each story from IPFS
     async function buildTextPreviews() {
-      if (storyPreviewMetadatas && storyPreviewMetadatas.length > 0) {
+      if (storyPreviewMetadatas && Object.keys(storyPreviewMetadatas).length > 0) {
         try {
           const textPreviewObjects = await Promise.all(
-            storyPreviewMetadatas.map(async previewMetadataObject => {
+            Object.keys(storyPreviewMetadatas).map(async key => {
               try {
+                const previewMetadataObject = storyPreviewMetadatas[key];
                 // Extract text cid
                 const id = previewMetadataObject.id;
                 const textCID = id.substring(0, id.length / 2); // because text comes first
@@ -147,12 +152,16 @@ function Read({ address, yourLocalBalance, readContracts, auth, writeContracts, 
       {storyPreviews && storyPreviews.length > 0 ? (
         storyPreviews.map(obj => {
           return (
-            <div style={{ margin: "10px" }} id={obj.id}>
-              <div>Story preview {obj.textPreview}</div>
-              <div>Upvotes: {obj.upvoteCount}</div>
-              <div>Downvotes: {obj.downvoteCount}</div>
-              {purchasedStoryIds.has(obj.id) ? <div>PURCHASED ALREADY</div> : null}
-            </div>
+            <a>
+              <button>
+                <div style={{ margin: "10px" }} id={obj.id}>
+                  <div>Story preview {obj.textPreview}</div>
+                  <div>Upvotes: {obj.upvoteCount}</div>
+                  <div>Downvotes: {obj.downvoteCount}</div>
+                  {purchasedStoryIds.has(obj.id) ? <div>PURCHASED ALREADY</div> : null}
+                </div>
+              </button>
+            </a>
           );
         })
       ) : (
