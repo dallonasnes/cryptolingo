49a1e9ccb3324a7d73f59df4bad60498611cd45e
Dallon Asnes <dallon.asnes@gmail.com>
2022-04-11 00:54:36 -0300

    Add react native package

---
+++ packages/react-native-app/App.tsx
@@ -0,0 +1,36 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { StatusBar, useColorScheme } from "react-native";
+import SplashScreen from "react-native-splash-screen";
+/**
+ * ? Local Imports
+ */
+import Navigation from "./src/services/navigation";
+import { isAndroid } from "@freakycoder/react-native-helpers";
+
+console.disableYellowBox = true;
+
+const App = () => {
+  const scheme = useColorScheme();
+  const isDarkMode = scheme === "dark";
+
+  React.useEffect(() => {
+    StatusBar.setBarStyle(isDarkMode ? "light-content" : "dark-content");
+    if (isAndroid) {
+      StatusBar.setBackgroundColor("rgba(0,0,0,0)");
+      StatusBar.setTranslucent(true);
+    }
+
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 750);
+  }, [scheme, isDarkMode]);
+
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+};
+
+export default App;
